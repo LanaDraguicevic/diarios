e1ad51aabf39f98b35ec50ca792230f32e56a6af
LanaDraguicevic <sdraguicevic@usm.cl>
2024-10-23 23:06:57 -0300

    arreglo cuestionario localidad

--- diarios_app/forms.py
+++ diarios_app/forms.py
@@ -10,6 +10,6 @@ class Busqueda(forms.Form):
     titulo = forms.CharField(required=False, label='Título')
     fecha_inicio = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}), label='Fecha Inicio')
     fecha_fin = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}), label='Fecha Fin')
-    localidad = forms.ChoiceField(required=False, label='Localidad')
+    localidad = forms.ChoiceField(required=False, label='Localidad', choices=LOCALIDAD_CHOICES)
 
 
