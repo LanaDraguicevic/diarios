9b3a57b65122f5079de817d3b835a0a00e068ead
Francisco20067 <favila@usm.cl>
2024-11-15 20:15:10 -0300

    Update admin.py

--- diarios_app/admin.py
+++ diarios_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import DiarioSolicitud
+
+class DiarioSolicitudAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'diario', 'fecha_solicitud', 'devuelto')
+    actions = ['marcar_como_devuelto']
+
+    def marcar_como_devuelto(self, request, queryset):
+        queryset.update(devuelto=True)
+        self.message_user(request, "Diarios marcados como devueltos.")
+    marcar_como_devuelto.short_description = 'Marcar como devuelto'
+
+admin.site.register(DiarioSolicitud, DiarioSolicitudAdmin)
+
 
 # Register your models here.
